11381ce508e11865cee776422d33ce06ad4a0bef
LockedInBasement <dariusz.strych.lib@gmail.com>
2025-07-04 16:01:37 +0200

    navbar fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Category } from './category/category';
+import { Todo } from './todo/todo';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component:Category},
+    {path:'todo', component:Todo}
+];
